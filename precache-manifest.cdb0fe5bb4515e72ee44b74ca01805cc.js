2da2944f8922865d37ef8715ceeb5fdb045e640d
paul <wichit.s@ubu.ac.th>
2020-09-02 11:22:56 +0700

    Updates

---
+++ precache-manifest.cdb0fe5bb4515e72ee44b74ca01805cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9fc251cadebe2d3b22a93857df120e1",
+    "url": "/commathweb/index.html"
+  },
+  {
+    "revision": "bce735068ba360f71a7c",
+    "url": "/commathweb/static/css/main.59bc23ef.chunk.css"
+  },
+  {
+    "revision": "a5de64d64bdf3390f4a3",
+    "url": "/commathweb/static/js/2.819af240.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/commathweb/static/js/2.819af240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce735068ba360f71a7c",
+    "url": "/commathweb/static/js/main.d97b5f56.chunk.js"
+  },
+  {
+    "revision": "3323733d28522f7a5ab5",
+    "url": "/commathweb/static/js/runtime-main.13f78efc.js"
+  }
+]);
